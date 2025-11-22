2164f127593d4c6ebfe7594f6dc14ad0cf1b6854
Neha <neha27970@gmail.com>
2025-11-22 14:57:28 +0530

    Changes to Support Page

--- src/landingPage/support/components/Hero.jsx
+++ src/landingPage/support/components/Hero.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 const Hero = () => {
   return (
-    <div className="container-fluid" id="supportHero">
+    <section className="container-fluid" id="supportHero">
       <div className="row  mb-5" id="supportWrapper">
         <h3 className="">Support</h3>
         <Link to="#" className="fs-5">My Ticket</Link>
       </div>
-    </div>
+    </section>
   )
 }
 
